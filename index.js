17c2085b07a257aa1de3640ba1555036e8b9a8ab
MWise707 <markezrawiseman@gmail.com>
2023-11-16 13:50:29 -0800

    Add event listener and button functionality - Step 1

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-(function() {
-  'use strict';
+'use strict';
 
-  // YOUR CODE HERE
-})();
+const stopBtn = document.getElementById("stopButton");
+const slowBtn = document.getElementById("slowButton");
+const goBtn = document.getElementById("goButton");
+
+const stopLight = document.getElementById("stopLight");
+const slowLight = document.getElementById("slowLight");
+const goLight = document.getElementById("goLight");
+
+
+console.log(stopLight);
+
+stopBtn.addEventListener('click', () => {
+  stopLight.classList.toggle("stop");
+});
+
+slowBtn.addEventListener('click', () => {
+  slowLight.classList.toggle("slow");
+});
+
+goBtn.addEventListener('click', () => {
+  goLight.classList.toggle("go");
+});
+
+// var x = createelement html <div>
+// x.style.backgroundcolor
+// htmlTAG.
+
+
+
+
+
+
+// comment out later
+/*
+const span = document.querySelector("span");
+const classes = span.classList;
+
+span.addEventListener("click", () => {
+  const result = classes.toggle("c");
+  span.textContent = `'c' ${
+    result ? "added" : "removed"
+  }; classList is now "${classes}".`;
+});
+*/
